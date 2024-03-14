83da0e980a183050863237eb882c61197f36d83c
Cimar-Rodrigo <crmorales1@umsa.bo>
2024-03-14 01:31:00 -0400

    Actualizando .env

--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 import { createPool } from "mysql2";
 
 export const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "test",
-    port: 3306,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "test",
+    port: procces.env.DB_PORT || 3306
 });
 
