22f4ec2f2b2669f89e261f01dfb85066d084a139
Cimar-Rodrigo <crmorales1@umsa.bo>
2024-03-21 00:16:37 -0400

    subiendo cors

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import authRoutes from './routes/auth.js'
 import cors from 'cors'
 const app = express()
 dotenv.config()
-// app.use(cors())
-app.use(cors({
-  origin: 'https://sistdonacion-production.up.railway.app', // Permitir solicitudes solo desde este origen
-  methods: ['GET', 'POST'], // Permitir solo ciertos métodos HTTP// Permitir solo ciertos encabezados
-}));
+const corsOptions ={
+  origin:'*', 
+  credentials:true,       
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
