dabc759008386ea40b031eb9cea2db65b48b920c
Cimar-Rodrigo <crmorales1@umsa.bo>
2024-03-21 01:26:46 -0400

    Subiendo orgDon

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,14 +54,14 @@ export const crearUsuario = async (req, res = response) => {
             await pool.query(`insert into donante(id_user) values (${datos[0][0].id_user})`)
             const {direccion_dn, lat, lng} = req.body
             console.log(direccion_dn, lat, lng)
-            await pool.query(`insert into donante_natural(id_user, direccion_dn, latitud_dn, longitud_dn) values (${datos[0][0].id_user}, '${direccion_dn}', ${lat}, ${lng})`)
+            await pool.query(`insert into donante_natural(id_user, direccion_dn, latitud_dn, longitud_dn) values (${datos[0][0].id_user}, '${direccion_dn}', '${lat}', '${lng}')`)
         }
         else if(tipo === 'orgDonante'){
             await pool.query(`insert into donante(id_user) values (${datos[0][0].id_user})`)
             const {nombre_od, tipo_od, lat, lng, direccion_od, nit_od, puesto_trabajo_d} = req.body
-            await pool.query(`insert into organizacion_donante(nombre_od, tipo_od, latitud_od, longitud_od, direccion_od, nit_od) values (${datos[0][0].id_user}, '${nombre_od}', '${tipo_od}', ${lat}, ${lng}, '${direccion_od}', '${nit_od}')`)
+            await pool.query(`insert into organizacion_donante(nombre_od, tipo_od, latitud_od, longitud_od, direccion_od, nit_od) values (${datos[0][0].id_user}, '${nombre_od}', '${tipo_od}', '${lat}', '${lng}', '${direccion_od}', '${nit_od}')`)
             const idOrg = await pool.query(`select id_org_don from organizacion_donante where nit_od = ${nit_od}`)
-            await pool.query(`insert into encargado_donante(id_user, puesto_trabajo_d, id_org_don) values (${datos[0][0].id_user}, ${puesto_trabajo_d}, ${idOrg})`)
+            await pool.query(`insert into encargado_donante(id_user, puesto_trabajo_d, id_org_don) values (${datos[0][0].id_user}, '${puesto_trabajo_d}', ${idOrg})`)
         }   
         else if(tipo === 'Receptor'){
             await pool.query(`insert into receptor(id_user) values (${datos[0][0].id_user})`)
@@ -73,7 +73,7 @@ export const crearUsuario = async (req, res = response) => {
         
         
         // Generar nuestro JWT
-        const token = await generarJWT(datos[0][0].id_user, datos[0][0].user, tipo)
+        const token = await generarJWT(datos[0][0].id_user, datos[0][0].user)
 
         res.status(201).json({
             ok: true,
@@ -120,9 +120,8 @@ export const loginUsuario = async (req, res = response) => {
                 msg: 'Password incorrecto'
             });
         }
-        const tipo = ""
         // Generar nuestro JWT
-        const token = await generarJWT(result[0][0].id_user, result[0][0].user, tipo)
+        const token = await generarJWT(result[0][0].id_user, result[0][0].user)
 
 
         res.json({
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,14 +2,14 @@ import jtw from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const generarJWT = ( uid, name, tipo ) => {
+export const generarJWT = ( uid, name ) => {
 
     //console.log(process.env)
     //console.log(process.env.SECRET_JWT_SEED)
 
     return new Promise((resolve, reject) => {
         
-        const payload = { uid, name, tipo };
+        const payload = { uid, name };
         
         jtw.sign( payload, process.env.SECRET_JWT_SEED, {
             expiresIn: '2h',
